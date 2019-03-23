466a6d1428b16710f551653187da11a786f96300
avasid <avasid@bt.iitr.ac.in>
2019-03-23 22:00:28 +0530

    Formatted the code

--- src/train.py
+++ src/train.py
@@ -1,153 +1,150 @@
-import keras
-from keras.layers import Dense, Conv2D, BatchNormalization, Activation
-from keras.layers import AveragePooling2D, MaxPooling2D, Input, Flatten, GlobalAveragePooling2D
+from keras import backend as tf
+from keras.callbacks import ModelCheckpoint, LearningRateScheduler, ReduceLROnPlateau
+from keras.layers import Dense, Conv2D, BatchNormalization, Activation, add
+from keras.layers import MaxPooling2D, Input, GlobalAveragePooling2D
+from keras.models import Model
 from keras.optimizers import Adam
 from keras.regularizers import l2
-from keras import backend as tf
-from keras.models import Model,load_model
-from keras.callbacks import ModelCheckpoint,LearningRateScheduler,ReduceLROnPlateau
-from utils import plot_model,getdata
-import numpy as np
+from utils import plot_model, getdata
 
 
 def training(RESOLUTION):
-    data_train,box_train,data_test,box_test=getdata()
+    data_train, box_train, data_test, box_test = getdata()
 
     # metric function
-    def my_metric(labels,predictions):
-        threshhold=0.75
-        x=predictions[:,0]*RESOLUTION
-        x=tf.maximum(tf.minimum(x,RESOLUTION),0.0)
-        y=predictions[:,1]*RESOLUTION
-        y=tf.maximum(tf.minimum(y,RESOLUTION),0.0)
-        width=predictions[:,2]*RESOLUTION
-        width=tf.maximum(tf.minimum(width,RESOLUTION),0.0)
-        height=predictions[:,3]*RESOLUTION
-        height=tf.maximum(tf.minimum(height,RESOLUTION),0.0)
-        label_x=labels[:,0]
-        label_y=labels[:,1]
-        label_width=labels[:,2]
-        label_height=labels[:,3]
-        a1=tf.tf.multiply(width,height)
-        a2=tf.tf.multiply(label_width,label_height)
-        x1=tf.maximum(x,label_x)
-        y1=tf.maximum(y,label_y)
-        x2=tf.minimum(x+width,label_x+label_width)
-        y2=tf.minimum(y+height,label_y+label_height)
-        IoU=tf.abs(tf.tf.multiply((x1-x2),(y1-y2)))/(a1+a2-tf.abs(tf.tf.multiply((x1-x2),(y1-y2))))
-        condition=tf.less(threshhold,IoU)
-        sum=tf.tf.where(condition,tf.ones(tf.shape(condition)),tf.zeros(tf.shape(condition)))
+    def my_metric(labels, predictions):
+        threshold = 0.75
+        x = predictions[:, 0] * RESOLUTION
+        x = tf.maximum(tf.minimum(x, RESOLUTION), 0.0)
+        y = predictions[:, 1] * RESOLUTION
+        y = tf.maximum(tf.minimum(y, RESOLUTION), 0.0)
+        width = predictions[:, 2] * RESOLUTION
+        width = tf.maximum(tf.minimum(width, RESOLUTION), 0.0)
+        height = predictions[:, 3] * RESOLUTION
+        height = tf.maximum(tf.minimum(height, RESOLUTION), 0.0)
+        label_x = labels[:, 0]
+        label_y = labels[:, 1]
+        label_width = labels[:, 2]
+        label_height = labels[:, 3]
+        a1 = tf.tf.multiply(width, height)
+        a2 = tf.tf.multiply(label_width, label_height)
+        x1 = tf.maximum(x, label_x)
+        y1 = tf.maximum(y, label_y)
+        x2 = tf.minimum(x + width, label_x + label_width)
+        y2 = tf.minimum(y + height, label_y + label_height)
+        IoU = tf.abs(tf.tf.multiply((x1 - x2), (y1 - y2))) / (a1 + a2 - tf.abs(tf.tf.multiply((x1 - x2), (y1 - y2))))
+        condition = tf.less(threshold, IoU)
+        sum = tf.tf.where(condition, tf.ones(tf.shape(condition)), tf.zeros(tf.shape(condition)))
         return tf.tf.reduce_mean(sum)
 
-    # loss functiongit
-    def smooth_l1_loss(true_box,pred_box):
-        loss=0.0
+    # loss function
+    def smooth_l1_loss(true_box, pred_box):
+        loss = 0.0
         for i in range(4):
-            residual=tf.abs(true_box[:,i]-pred_box[:,i]*RESOLUTION)
-            condition=tf.less(residual,1.0)
-            small_res=0.5*tf.square(residual)
-            large_res=residual-0.5
-            loss=loss+tf.tf.where(condition,small_res,large_res)
+            residual = tf.abs(true_box[:, i] - pred_box[:, i] * RESOLUTION)
+            condition = tf.less(residual, 1.0)
+            small_res = 0.5 * tf.square(residual)
+            large_res = residual - 0.5
+            loss = loss + tf.tf.where(condition, small_res, large_res)
         return tf.tf.reduce_mean(loss)
 
-
-    def resnet_block(inputs,num_filters,kernel_size,strides,activation='relu'):
-        x=Conv2D(num_filters,kernel_size=kernel_size,strides=strides,padding='same',kernel_initializer='he_normal',kernel_regularizer=l2(1e-3))(inputs)
-        x=BatchNormalization()(x)
-        if(activation):
-            x=Activation('relu')(x)
+    def resnet_block(inputs, num_filters, kernel_size, strides, activation='relu'):
+        x = Conv2D(num_filters, kernel_size=kernel_size, strides=strides, padding='same',
+                   kernel_initializer='he_normal', kernel_regularizer=l2(1e-3))(inputs)
+        x = BatchNormalization()(x)
+        if (activation):
+            x = Activation('relu')(x)
         return x
 
-
     def resnet18():
-        inputs=Input((RESOLUTION,RESOLUTION,3))
+        inputs = Input((RESOLUTION, RESOLUTION, 3))
 
         # conv1
-        x=resnet_block(inputs,64,[7,7],2)
+        x = resnet_block(inputs, 64, [7, 7], 2)
 
         # conv2
-        x=MaxPooling2D([3,3],2,'same')(x)
+        x = MaxPooling2D([3, 3], 2, 'same')(x)
         for i in range(2):
-            a=resnet_block(x,64,[3,3],1)
-            b=resnet_block(a,64,[3,3],1,activation=None)
-            x=keras.layers.add([x,b])
-            x=Activation('relu')(x)
+            a = resnet_block(x, 64, [3, 3], 1)
+            b = resnet_block(a, 64, [3, 3], 1, activation=None)
+            x = add([x, b])
+            x = Activation('relu')(x)
 
         # conv3
-        a=resnet_block(x,128,[1,1],2)
-        b=resnet_block(a,128,[3,3],1,activation=None)
-        x=Conv2D(128,kernel_size=[1,1],strides=2,padding='same',kernel_initializer='he_normal',kernel_regularizer=l2(1e-3))(x)
-        x=keras.layers.add([x,b])
-        x=Activation('relu')(x)
+        a = resnet_block(x, 128, [1, 1], 2)
+        b = resnet_block(a, 128, [3, 3], 1, activation=None)
+        x = Conv2D(128, kernel_size=[1, 1], strides=2, padding='same', kernel_initializer='he_normal',
+                   kernel_regularizer=l2(1e-3))(x)
+        x = add([x, b])
+        x = Activation('relu')(x)
 
-        a=resnet_block(x,128,[3,3],1)
-        b=resnet_block(a,128,[3,3],1,activation=None)
-        x=keras.layers.add([x,b])
-        x=Activation('relu')(x)
+        a = resnet_block(x, 128, [3, 3], 1)
+        b = resnet_block(a, 128, [3, 3], 1, activation=None)
+        x = add([x, b])
+        x = Activation('relu')(x)
 
         # conv4
-        a=resnet_block(x,256,[1,1],2)
-        b=resnet_block(a,256,[3,3],1,activation=None)
-        x=Conv2D(256,kernel_size=[1,1],strides=2,padding='same',kernel_initializer='he_normal',kernel_regularizer=l2(1e-3))(x)
-        x=keras.layers.add([x,b])
-        x=Activation('relu')(x)
+        a = resnet_block(x, 256, [1, 1], 2)
+        b = resnet_block(a, 256, [3, 3], 1, activation=None)
+        x = Conv2D(256, kernel_size=[1, 1], strides=2, padding='same', kernel_initializer='he_normal',
+                   kernel_regularizer=l2(1e-3))(x)
+        x = add([x, b])
+        x = Activation('relu')(x)
 
-        a=resnet_block(x,256,[3,3],1)
-        b=resnet_block(a,256,[3,3],1,activation=None)
-        x=keras.layers.add([x,b])
-        x=Activation('relu')(x)
+        a = resnet_block(x, 256, [3, 3], 1)
+        b = resnet_block(a, 256, [3, 3], 1, activation=None)
+        x = add([x, b])
+        x = Activation('relu')(x)
 
         # conv5
-        a=resnet_block(x,512,[1,1],2)
-        b=resnet_block(a,512,[3,3],1,activation=None)
-        x=Conv2D(512,kernel_size=[1,1],strides=2,padding='same',kernel_initializer='he_normal',kernel_regularizer=l2(1e-3))(x)
-        x=keras.layers.add([x,b])
-        x=Activation('relu')(x)
+        a = resnet_block(x, 512, [1, 1], 2)
+        b = resnet_block(a, 512, [3, 3], 1, activation=None)
+        x = Conv2D(512, kernel_size=[1, 1], strides=2, padding='same', kernel_initializer='he_normal',
+                   kernel_regularizer=l2(1e-3))(x)
+        x = add([x, b])
+        x = Activation('relu')(x)
 
-        a=resnet_block(x,512,[3,3],1)
-        b=resnet_block(a,512,[3,3],1,activation=None)
-        x=keras.layers.add([x,b])
-        x=Activation('relu')(x)
-        y=GlobalAveragePooling2D(data_format="channels_last")(x)
-        # x=AveragePooling2D(pool_size=4,data_format="channels_last")(x)
-        # out:1*1*512
+        a = resnet_block(x, 512, [3, 3], 1)
+        b = resnet_block(a, 512, [3, 3], 1, activation=None)
+        x = add([x, b])
+        x = Activation('relu')(x)
+        y = GlobalAveragePooling2D(data_format="channels_last")(x)
 
-        # y=Flatten()(x)
         # out:512
-        y=Dense(1000,kernel_initializer='he_normal',kernel_regularizer=l2(1e-3))(y)
-        outputs=Dense(4,kernel_initializer='he_normal',kernel_regularizer=l2(1e-3))(y)
+        y = Dense(1000, kernel_initializer='he_normal', kernel_regularizer=l2(1e-3))(y)
+        outputs = Dense(4, kernel_initializer='he_normal', kernel_regularizer=l2(1e-3))(y)
 
-        model=Model(inputs=inputs,outputs=outputs)
+        model = Model(inputs=inputs, outputs=outputs)
         return model
 
     model = resnet18()
 
-
-    model.compile(loss=smooth_l1_loss,optimizer=Adam(),metrics=[my_metric])
+    model.compile(loss=smooth_l1_loss, optimizer=Adam(), metrics=[my_metric])
 
     model.summary()
 
     def lr_sch(epoch):
-        #200 total
-        if epoch <50:
+        # 200 total
+        if epoch < 50:
             return 1e-3
-        if 50<=epoch<100:
+        if 50 <= epoch < 100:
             return 1e-4
-        if epoch>=100:
+        if epoch >= 100:
             return 1e-5
 
-    lr_scheduler=LearningRateScheduler(lr_sch)
-    lr_reducer=ReduceLROnPlateau(monitor='val_my_metric',factor=0.2,patience=5,mode='max',min_lr=1e-3)
+    lr_scheduler = LearningRateScheduler(lr_sch)
+    lr_reducer = ReduceLROnPlateau(monitor='val_my_metric', factor=0.2, patience=5, mode='max', min_lr=1e-3)
 
-    checkpoint=ModelCheckpoint('model.h5',monitor='val_loss',verbose=0,save_best_only=True,mode='auto')
+    checkpoint = ModelCheckpoint('model.h5', monitor='val_loss', verbose=0, save_best_only=True, mode='auto')
 
-    model_details=model.fit(data_train,box_train,batch_size=128,epochs=100,shuffle=True,validation_split=0.1,callbacks=[lr_scheduler,lr_reducer,checkpoint],verbose=1)
+    model_details = model.fit(data_train, box_train, batch_size=128, epochs=100, shuffle=True, validation_split=0.1,
+                              callbacks=[lr_scheduler, lr_reducer, checkpoint], verbose=1)
 
     model.save('model.h5')
 
-    scores=model.evaluate(data_test,box_test,verbose=1)
-    print('Test loss : ',scores[0])
-    print('Test accuracy : ',scores[1])
+    scores = model.evaluate(data_test, box_test, verbose=1)
+    print('Test loss : ', scores[0])
+    print('Test accuracy : ', scores[1])
 
     plot_model(model_details)
     return model
--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,6 @@
-import numpy as np
 import matplotlib
+import numpy as np
+
 matplotlib.use('agg')
 import matplotlib.pyplot as plt
 import pickle
@@ -9,34 +10,33 @@ import random
 def getdata():
     # read data and shuffle
 
-    f=open("./id_to_data","rb+")
-    data=pickle.load(f)
+    f = open("./id_to_data", "rb+")
+    data = pickle.load(f)
     f.close()
 
     # Shuffle
     len_data = len(data)
-    index=[i for i in range(len_data)]
+    index = [i for i in range(len_data)]
     random.shuffle(index)
 
-    data=data[index]
-    split_boundary = int(len_data*0.75) # 0.75 split ratio
-    data_train=data[0:split_boundary]
-    data_test=data[split_boundary:]
+    data = data[index]
+    split_boundary = int(len_data * 0.75)  # 0.75 split ratio
+    data_train = data[0:split_boundary]
+    data_test = data[split_boundary:]
 
-    f=open("./id_to_box","rb+")
-    box=pickle.load(f)
+    f = open("./id_to_box", "rb+")
+    box = pickle.load(f)
     f.close()
 
-    box=box[index]
-    box_train=box[0:split_boundary]
-    box_test=box[split_boundary:]
+    box = box[index]
+    box_train = box[0:split_boundary]
+    box_test = box[split_boundary:]
 
     f = open("./id_to_mean", "rb+")
     mean = pickle.load(f)
     f.close()
 
     mean = mean[index]
-    # mean_train = mean[0:split_boundary]
     mean_test = mean[split_boundary:]
 
     f = open("./id_to_std", "rb+")
@@ -44,42 +44,42 @@ def getdata():
     f.close()
 
     std = std[index]
-    # std_train = std[0:split_boundary]
     std_test = std[split_boundary:]
 
     with open("./id_to_data_test", "wb+") as fh:
-        pickle.dump(data_test,fh ,protocol=4)
+        pickle.dump(data_test, fh, protocol=4)
 
     with open("./id_to_box_test", "wb+") as fh:
-        pickle.dump(box_test,fh ,protocol=4)
+        pickle.dump(box_test, fh, protocol=4)
 
     with open("./id_to_mean_test", "wb+") as fh:
-        pickle.dump(mean_test,fh ,protocol=4)
+        pickle.dump(mean_test, fh, protocol=4)
 
     with open("./id_to_std_test", "wb+") as fh:
-        pickle.dump(std_test,fh ,protocol=4)
+        pickle.dump(std_test, fh, protocol=4)
 
-    return data_train,box_train,data_test,box_test
+    return data_train, box_train, data_test, box_test
 
 
 def plot_model(model_details):
-    fig, axs = plt.subplots(1,2,figsize=(15,5))
+    fig, axs = plt.subplots(1, 2, figsize=(15, 5))
 
-    axs[0].plot(range(1,len(model_details.history['my_metric'])+1),model_details.history['my_metric'])
-    axs[0].plot(range(1,len(model_details.history['val_my_metric'])+1),[1.7*x for x in model_details.history['val_my_metric']])
+    axs[0].plot(range(1, len(model_details.history['my_metric']) + 1), model_details.history['my_metric'])
+    axs[0].plot(range(1, len(model_details.history['val_my_metric']) + 1),
+                [1.7 * x for x in model_details.history['val_my_metric']])
     axs[0].set_title('Model Accuracy')
     axs[0].set_ylabel('Accuracy')
     axs[0].set_xlabel('Epoch')
-    axs[0].set_xticks(np.arange(1,len(model_details.history['my_metric'])+1),len(model_details.history['my_metric'])/10)
+    axs[0].set_xticks(np.arange(1, len(model_details.history['my_metric']) + 1),
+                      len(model_details.history['my_metric']) / 10)
     axs[0].legend(['train', 'val'], loc='best')
 
-    axs[1].plot(range(1,len(model_details.history['loss'])+1),model_details.history['loss'])
-    axs[1].plot(range(1,len(model_details.history['val_loss'])+1),model_details.history['val_loss'])
+    axs[1].plot(range(1, len(model_details.history['loss']) + 1), model_details.history['loss'])
+    axs[1].plot(range(1, len(model_details.history['val_loss']) + 1), model_details.history['val_loss'])
     axs[1].set_title('Model Loss')
     axs[1].set_ylabel('Loss')
     axs[1].set_xlabel('Epoch')
-    axs[1].set_xticks(np.arange(1,len(model_details.history['loss'])+1),len(model_details.history['loss'])/10)
+    axs[1].set_xticks(np.arange(1, len(model_details.history['loss']) + 1), len(model_details.history['loss']) / 10)
     axs[1].legend(['train', 'val'], loc='best')
 
     plt.savefig("model.png")
-
