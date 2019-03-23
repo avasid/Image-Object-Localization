5890abe0b425cfb6a5bfaa520a7f01648d49b9ff
Aakif Vasid <aakif@aakif-ubuntu-16.04>
2019-03-23 22:00:28 +0530

    Formatted the code

--- object_localization.py
+++ object_localization.py
@@ -1,6 +1,9 @@
-import os, sys
-from keras.models import Model,load_model
+import os
+import sys
+
 from keras import backend as tf
+from keras.models import load_model
+
 sys.path.append(os.getcwd() + "/src")
 from images2array import convert
 from train import training
@@ -15,42 +18,45 @@ def run_all(RESOLUTION):
     model.save('./model.h5')
     model = load_model('./model.h5')
 
+
 # metric_function
-def my_metric(labels,predictions):
-    threshhold=0.75
-    x=predictions[:,0]*RESOLUTION
-    x=tf.maximum(tf.minimum(x,RESOLUTION),0.0)
-    y=predictions[:,1]*RESOLUTION
-    y=tf.maximum(tf.minimum(y,RESOLUTION),0.0)
-    width=predictions[:,2]*RESOLUTION
-    width=tf.maximum(tf.minimum(width,RESOLUTION),0.0)
-    height=predictions[:,3]*RESOLUTION
-    height=tf.maximum(tf.minimum(height,RESOLUTION),0.0)
-    label_x=labels[:,0]
-    label_y=labels[:,1]
-    label_width=labels[:,2]
-    label_height=labels[:,3]
-    a1=tf.tf.multiply(width,height)
-    a2=tf.tf.multiply(label_width,label_height)
-    x1=tf.maximum(x,label_x)
-    y1=tf.maximum(y,label_y)
-    x2=tf.minimum(x+width,label_x+label_width)
-    y2=tf.minimum(y+height,label_y+label_height)
-    IoU=tf.abs(tf.tf.multiply((x1-x2),(y1-y2)))/(a1+a2-tf.abs(tf.tf.multiply((x1-x2),(y1-y2))))
-    condition=tf.less(threshhold,IoU)
-    sum=tf.tf.where(condition,tf.ones(tf.shape(condition)),tf.zeros(tf.shape(condition)))
+def my_metric(labels, predictions):
+    threshhold = 0.75
+    x = predictions[:, 0] * RESOLUTION
+    x = tf.maximum(tf.minimum(x, RESOLUTION), 0.0)
+    y = predictions[:, 1] * RESOLUTION
+    y = tf.maximum(tf.minimum(y, RESOLUTION), 0.0)
+    width = predictions[:, 2] * RESOLUTION
+    width = tf.maximum(tf.minimum(width, RESOLUTION), 0.0)
+    height = predictions[:, 3] * RESOLUTION
+    height = tf.maximum(tf.minimum(height, RESOLUTION), 0.0)
+    label_x = labels[:, 0]
+    label_y = labels[:, 1]
+    label_width = labels[:, 2]
+    label_height = labels[:, 3]
+    a1 = tf.tf.multiply(width, height)
+    a2 = tf.tf.multiply(label_width, label_height)
+    x1 = tf.maximum(x, label_x)
+    y1 = tf.maximum(y, label_y)
+    x2 = tf.minimum(x + width, label_x + label_width)
+    y2 = tf.minimum(y + height, label_y + label_height)
+    IoU = tf.abs(tf.tf.multiply((x1 - x2), (y1 - y2))) / (a1 + a2 - tf.abs(tf.tf.multiply((x1 - x2), (y1 - y2))))
+    condition = tf.less(threshhold, IoU)
+    sum = tf.tf.where(condition, tf.ones(tf.shape(condition)), tf.zeros(tf.shape(condition)))
     return tf.tf.reduce_mean(sum)
 
+
 # loss_function
-def smooth_l1_loss(true_box,pred_box):
-    loss=0.0
+def smooth_l1_loss(true_box, pred_box):
+    loss = 0.0
     for i in range(4):
-        residual=tf.abs(true_box[:,i]-pred_box[:,i]*RESOLUTION)
-        condition=tf.less(residual,1.0)
-        small_res=0.5*tf.square(residual)
-        large_res=residual-0.5
-        loss=loss+tf.tf.where(condition,small_res,large_res)
+        residual = tf.abs(true_box[:, i] - pred_box[:, i] * RESOLUTION)
+        condition = tf.less(residual, 1.0)
+        small_res = 0.5 * tf.square(residual)
+        large_res = residual - 0.5
+        loss = loss + tf.tf.where(condition, small_res, large_res)
     return tf.tf.reduce_mean(loss)
+
 
 ##################################
 
@@ -71,12 +77,9 @@ while True:
         name = input("Enter model name: ")
         print("Loading model..." + name)
         try:
-            model = load_model(name, custom_objects1={'smooth_l1_loss': smooth_l1_loss, 'my_metric':my_metric})
+            model = load_model(name, custom_objects1={'smooth_l1_loss': smooth_l1_loss, 'my_metric': my_metric})
         except:
             print("\nError in loading model\n")
 
     else:
         choice_dict[choice](RESOLUTION)
-
-
-    print("Complete")
--- src/pred_on_images.py
+++ src/pred_on_images.py
@@ -1,102 +1,93 @@
 import pickle
-import numpy as np
+
 import matplotlib
+import numpy as np
+
 matplotlib.use('agg')
 import matplotlib.pyplot as plt
-import keras
 from keras.models import load_model
 import random
 from keras import backend as tf
-from utils import getdata
-# data_train,box_train,data_test,box_test=getdata()
+
 
 def predictions(RESOLUTION):
-    def my_metric(labels,predictions):
-        threshhold=0.75
-        x=predictions[:,0]*120
-        x=tf.maximum(tf.minimum(x,120.0),0.0)
-        y=predictions[:,1]*120
-        y=tf.maximum(tf.minimum(y,120.0),0.0)
-        width=predictions[:,2]*120
-        width=tf.maximum(tf.minimum(width,120.0),0.0)
-        height=predictions[:,3]*120
-        height=tf.maximum(tf.minimum(height,120.0),0.0)
-        label_x=labels[:,0]
-        label_y=labels[:,1]
-        label_width=labels[:,2]
-        label_height=labels[:,3]
-        a1=tf.tf.multiply(width,height)
-        a2=tf.tf.multiply(label_width,label_height)
-        x1=tf.maximum(x,label_x)
-        y1=tf.maximum(y,label_y)
-        x2=tf.minimum(x+width,label_x+label_width)
-        y2=tf.minimum(y+height,label_y+label_height)
-        IoU=tf.abs(tf.tf.multiply((x1-x2),(y1-y2)))/(a1+a2-tf.abs(tf.tf.multiply((x1-x2),(y1-y2))))
-        condition=tf.less(threshhold,IoU)
-        sum=tf.tf.where(condition,tf.ones(tf.shape(condition)),tf.zeros(tf.shape(condition)))
+    def my_metric(labels, predictions):
+        threshhold = 0.75
+        x = predictions[:, 0] * 120
+        x = tf.maximum(tf.minimum(x, 120.0), 0.0)
+        y = predictions[:, 1] * 120
+        y = tf.maximum(tf.minimum(y, 120.0), 0.0)
+        width = predictions[:, 2] * 120
+        width = tf.maximum(tf.minimum(width, 120.0), 0.0)
+        height = predictions[:, 3] * 120
+        height = tf.maximum(tf.minimum(height, 120.0), 0.0)
+        label_x = labels[:, 0]
+        label_y = labels[:, 1]
+        label_width = labels[:, 2]
+        label_height = labels[:, 3]
+        a1 = tf.tf.multiply(width, height)
+        a2 = tf.tf.multiply(label_width, label_height)
+        x1 = tf.maximum(x, label_x)
+        y1 = tf.maximum(y, label_y)
+        x2 = tf.minimum(x + width, label_x + label_width)
+        y2 = tf.minimum(y + height, label_y + label_height)
+        IoU = tf.abs(tf.tf.multiply((x1 - x2), (y1 - y2))) / (a1 + a2 - tf.abs(tf.tf.multiply((x1 - x2), (y1 - y2))))
+        condition = tf.less(threshhold, IoU)
+        sum = tf.tf.where(condition, tf.ones(tf.shape(condition)), tf.zeros(tf.shape(condition)))
         return tf.tf.reduce_mean(sum)
 
-
-
-    def smooth_l1_loss(true_box,pred_box):
-        loss=0.0
+    def smooth_l1_loss(true_box, pred_box):
+        loss = 0.0
         for i in range(4):
-            residual=tf.abs(true_box[:,i]-pred_box[:,i]*120)
-            condition=tf.less(residual,1.0)
-            small_res=0.5*tf.square(residual)
-            large_res=residual-0.5
-            loss=loss+tf.tf.where(condition,small_res,large_res)
+            residual = tf.abs(true_box[:, i] - pred_box[:, i] * 120)
+            condition = tf.less(residual, 1.0)
+            small_res = 0.5 * tf.square(residual)
+            large_res = residual - 0.5
+            loss = loss + tf.tf.where(condition, small_res, large_res)
         return tf.tf.reduce_mean(loss)
-
 
     plt.switch_backend('agg')
 
-    f=open("./id_to_data_test","rb+")
-    data=pickle.load(f)
+    f = open("./id_to_data_test", "rb+")
+    data = pickle.load(f)
 
-    f=open("./id_to_box_test","rb+")
-    box=pickle.load(f)
+    f = open("./id_to_box_test", "rb+")
+    box = pickle.load(f)
 
-    # f=open("./id_to_size","rb+")
-    # size=pickle.load(f)
+    f = open("./id_to_mean_test", "rb+")
+    mean = pickle.load(f)
 
-    f=open("./id_to_mean_test","rb+")
-    mean=pickle.load(f)
-
-    f=open("./id_to_std_test","rb+")
-    std=pickle.load(f)
+    f = open("./id_to_std_test", "rb+")
+    std = pickle.load(f)
 
     lenn = len(data)
-    index=[i for i in range(lenn)]
-    index=random.sample(index,100)
+    index = [i for i in range(lenn)]
+    index = random.sample(index, 100)
 
+    model = load_model('./model.h5', custom_objects={'smooth_l1_loss': smooth_l1_loss, 'my_metric': my_metric})
+    result = model.predict(data[index, :, :, :])
 
-    model=keras.models.load_model('./model.h5', custom_objects={'smooth_l1_loss': smooth_l1_loss, 'my_metric':my_metric})
-    result=model.predict(data[index,:,:,:])
-
-    # mean=[0.485,0.456,0.406]
-    # std=[0.229,0.224,0.225]
-    j=0
+    j = 0
     for i in index:
-        print("Predicting "+str(i)+"th image.")
-        true_box=box[i]
-        image=data[i]
-        prediction=result[j]
-        j+=1
+        print("Predicting " + str(i) + "th image.")
+        true_box = box[i]
+        image = data[i]
+        prediction = result[j]
+        j += 1
         for channel in range(3):
             meani = mean[i]
             stdi = std[i]
-            image[:,:,channel]=image[:,:,channel]*stdi[channel]+meani[channel]
+            image[:, :, channel] = image[:, :, channel] * stdi[channel] + meani[channel]
 
-        image=image*255
-        image=image.astype(np.uint8)
+        image = image * 255
+        image = image.astype(np.uint8)
         plt.imshow(image)
 
-
-        plt.gca().add_patch(plt.Rectangle((true_box[0],true_box[1]),true_box[2],true_box[3],fill=False,edgecolor='red',linewidth=2,alpha=0.5))
-        plt.gca().add_patch(plt.Rectangle((prediction[0]*RESOLUTION,prediction[1]*RESOLUTION),prediction[2]*RESOLUTION,prediction[3]*RESOLUTION,fill=False,edgecolor='green',linewidth=2,alpha=0.5))
+        plt.gca().add_patch(plt.Rectangle((true_box[0], true_box[1]), true_box[2], true_box[3],
+                                          fill=False, edgecolor='red', linewidth=2, alpha=0.5))
+        plt.gca().add_patch(plt.Rectangle((prediction[0] * RESOLUTION, prediction[1] * RESOLUTION),
+                                          prediction[2] * RESOLUTION, prediction[3] * RESOLUTION,
+                                          fill=False, edgecolor='green', linewidth=2, alpha=0.5))
         plt.show()
-        plt.savefig("./pred/"+str(i)+".png")
+        plt.savefig("./pred/" + str(i) + ".png")
         plt.cla()
-
-
